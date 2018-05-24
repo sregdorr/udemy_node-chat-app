e5e7caa4798d2e13f35890ab53b6f7120e30aab8
rrodgers <rrodgers0827@outlook.com>
2018-05-24 16:36:55 -0500

    wire up socket.io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3050;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Started on ${port}`);
 });
